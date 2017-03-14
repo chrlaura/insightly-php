1e47602593ac9131e0bbb80e43c051a85fc2fab9
Christian Rasmussen <chrlaura@gmail.com>
2017-03-14 15:47:43 +0100

    Get contact json from API

---
+++ src/Models/Contact.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: christian
+ * Date: 14/03/17
+ * Time: 14:44
+ */
+
+namespace Insightly\Models;
+
+
+class Contact
+{
+
+    const ROUTE = "/Contacts";
+
+    public function __construct(array $contactData) {
+
+    }
+
+}
